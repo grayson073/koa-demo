5bdecd003fafebc39d702ac8bae84a731949b110
Kevin Grayson <k.grayson.ms@gmail.com>
2019-02-02 15:54:08 -0800

    Error handler test successful

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const koaRouter = require('koa-router');
 const app = new koa();
 const router = new koaRouter();
 
+app.use(async(ctx, next) => {
+  try {
+    await next();
+  }
+  catch(err) {
+    console.error(err.status, err.message);
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+  }
+});
+
+// Numeric argument must be passed first to ctx.throw (e.g. 500)
 router.get('test', '/', ctx => {
+  ctx.throw(500, 'TESTING ERROR');
   ctx.body = 'Testing Koa router!';
 });
 
